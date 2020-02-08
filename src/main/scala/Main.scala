1de9d6199acc0f98d8b8ccee8768356a334e8570
mtnori <moto44.noriyuki@gmail.com>
2020-02-08 09:43:41 +0900

    Add Right or Left sample

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
-
-import sample.{Foo, Qux}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import sample.{Foo, Qux, Sample}
 
 object Main extends App {
   val foo: Foo = Qux(13, Some(14.0))
@@ -10,4 +12,11 @@ object Main extends App {
 
   val decodedFoo = decode[Foo](json)
   println(decodedFoo)
+
+  println(Sample.parse1(Sample.successJson))
+
+  Sample.parse1(Sample.failureJson) match {
+    case Left(failure) => println("Invalid Json :(")
+    case Right(json)   => println("Yay, got some JSON!")
+  }
 }
--- src/main/scala/sample/Sample.scala
+++ src/main/scala/sample/Sample.scala
@@ -1,5 +1,23 @@
 package sample
 
+import io.circe._, io.circe.parser._
+
 sealed trait Foo
 case class Bar(xs: Vector[String]) extends Foo
 case class Qux(i: Int, d: Option[Double]) extends Foo
+
+object Sample {
+
+  val successJson: String = """
+{
+  "foo": "bar",
+  "baz": 123,
+  "list of stuff": [ 4, 5, 6 ]
+}
+"""
+
+  val failureJson: String = "yolo"
+
+  def parse1(rawJson: String) = parse(rawJson)
+
+}
